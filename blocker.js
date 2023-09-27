12f093f38d23c8d079799cb2f2d2deeccf1229cb
ethandoan <ethan.doan@wilink.de>
2023-09-27 18:12:37 -0500

    Added Event Listeners for Button

--- blocker.js
+++ blocker.js
@@ -1,4 +1,8 @@
 "use strict";
 document.getElementById('blockSite').addEventListener('click', function() {
-    
+    let url = document.getElementById('siteToBlock').ariaValueText;
+
+    chrome.storage.set({[url]: true}, function() {
+        console.log(siteToBlock + ' is blocked.');
+    });
 });
